c74580edfc6b99e4973e49e1abaf4f1293b8af11
mahmoudashrafmohamed <ashraaf.mahmoud@gmail.com>
2022-04-17 17:06:49 +0200

    add ItemsOfTagAdapter and ItemOfTagModel

--- app/src/main/java/com/mahmoud_ashraf/menustask/menu/adapters/ItemsOfTagAdapter.kt
+++ app/src/main/java/com/mahmoud_ashraf/menustask/menu/adapters/ItemsOfTagAdapter.kt
@@ -9,34 +9,34 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.mahmoud_ashraf.domain.menu.models.TagsModel
 import com.mahmoud_ashraf.menustask.R
-import com.mahmoud_ashraf.menustask.databinding.ItemTagBinding
+import com.mahmoud_ashraf.menustask.databinding.ItemItemsOfTagBinding
 
-class TagsAdapter :
-    ListAdapter<TagsModel, TagsViewHolder>(TagsDiffCallback()) {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TagsViewHolder {
-        return TagsViewHolder(
-            ItemTagBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+class ItemsOfTagAdapter :
+    ListAdapter<TagsModel, ItemOfTagViewHolder>(ItemOfTagDiffCallback()) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemOfTagViewHolder {
+        return ItemOfTagViewHolder(
+            ItemItemsOfTagBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         )
     }
-    override fun onBindViewHolder(holder: TagsViewHolder, position: Int) =
+    override fun onBindViewHolder(holder: ItemOfTagViewHolder, position: Int) =
         holder.bind(getItem(position))
 
 }
 
-class TagsViewHolder(
-    private val binding: ItemTagBinding
+class ItemOfTagViewHolder(
+    private val binding: ItemItemsOfTagBinding
 ) : RecyclerView.ViewHolder(binding.root) {
     fun bind(
         data: TagsModel
     ) {
        with(binding){
-           Glide.with(ivTag.context).load(Uri.parse(data.photoURL)).placeholder(R.drawable.img_placeholder).into(ivTag)
-           tvTagName.text = data.tagName
+           Glide.with(ivItem.context).load(Uri.parse(data.photoURL)).placeholder(R.drawable.img_placeholder).into(ivItem)
+           tvItemName.text = data.tagName
        }
     }
 }
 
-class TagsDiffCallback : DiffUtil.ItemCallback<TagsModel>() {
+class ItemOfTagDiffCallback : DiffUtil.ItemCallback<TagsModel>() {
     override fun areItemsTheSame(
         oldItem: TagsModel,
         newItem: TagsModel
---
+++ domain/src/main/java/com/mahmoud_ashraf/domain/menu/models/ItemOfTagModel.kt
@@ -0,0 +1,8 @@
+package com.mahmoud_ashraf.domain.menu.models
+
+data class ItemOfTagModel(
+    val id: Int,
+    val name: String,
+    val description: String,
+    val photoUrl: String
+)
