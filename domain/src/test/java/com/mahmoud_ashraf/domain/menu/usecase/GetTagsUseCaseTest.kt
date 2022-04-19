11685878c1ba3358883e3dfeacde3c427079cb96
mahmoudashrafmohamed <ashraaf.mahmoud@gmail.com>
2022-04-19 05:18:18 +0200

    add unit test for GetTagsUseCase

---
+++ domain/src/test/java/com/mahmoud_ashraf/domain/menu/usecase/GetTagsUseCaseTest.kt
@@ -0,0 +1,89 @@
+package com.mahmoud_ashraf.domain.menu.usecase
+
+import com.mahmoud_ashraf.domain.menu.models.TagsModel
+import com.mahmoud_ashraf.domain.menu.repository.MenuRepository
+import com.nhaarman.mockito_kotlin.any
+import io.reactivex.Single
+import org.junit.Test
+import org.mockito.Mockito
+import java.net.UnknownHostException
+
+class GetTagsUseCaseTest {
+
+    @Test
+    fun `GetTagsUseCase invoke() with page will invoke MenuRepository`() {
+        // arrange
+        val page = "1"
+        val tags = listOf(
+            TagsModel(
+                "4 - Deserts",
+                "https://s3-eu-west-1.amazonaws.com//elmenusv5/Normal/b2276d5d-27b7-11e8-add5-0242ac110011.jpg"
+            ),
+            TagsModel(
+                "4 - Egyptian",
+                "https://s3-eu-west-1.amazonaws.com//elmenusv5/Normal/b2276d5d-27b7-11e8-add5-0242ac110011.jpg"
+            ),
+        )
+        val repository = Mockito.mock(MenuRepository::class.java)
+        Mockito.`when`(repository.getTags(any())).thenReturn(
+            Single.just(tags)
+        )
+
+        val getTagsUseCase = GetTagsUseCase(repository)
+
+        // act
+        val resultObserver = getTagsUseCase(page).test()
+
+        // assert
+        Mockito.verify(repository).getTags(page)
+        resultObserver.dispose()
+    }
+
+    @Test
+    fun `GetTagsUseCase invoke() with page then return Single of ListOf tagsModel`() {
+        // arrange
+        val page = "1"
+        val tags = listOf(
+            TagsModel(
+                "4 - Deserts",
+                "https://s3-eu-west-1.amazonaws.com//elmenusv5/Normal/b2276d5d-27b7-11e8-add5-0242ac110011.jpg"
+            ),
+            TagsModel(
+                "4 - Egyptian",
+                "https://s3-eu-west-1.amazonaws.com//elmenusv5/Normal/b2276d5d-27b7-11e8-add5-0242ac110011.jpg"
+            ),
+        )
+        val repository = Mockito.mock(MenuRepository::class.java)
+        Mockito.`when`(repository.getTags(any())).thenReturn(
+            Single.just(tags)
+        )
+
+        val getTagsUseCase = GetTagsUseCase(repository)
+
+        // act
+        val resultObserver = getTagsUseCase(page).test()
+
+        // assert
+        resultObserver.assertValue(tags)
+        resultObserver.dispose()
+    }
+
+    @Test
+    fun `GetTagsUseCase invoke() with page then return Error`() {
+        // arrange
+        val page = "1"
+        val exception = UnknownHostException()
+        val repository = Mockito.mock(MenuRepository::class.java)
+        Mockito.`when`(repository.getTags(any())).thenReturn(
+            Single.error(exception)
+        )
+        val getTagsUseCase = GetTagsUseCase(repository)
+        // act
+        val resultObserver = getTagsUseCase(page).test()
+
+        // assert
+        resultObserver.assertError(exception)
+        resultObserver.dispose()
+    }
+
+}
